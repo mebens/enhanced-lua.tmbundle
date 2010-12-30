a5bcb420f834cc8b2e5b692c6efef320f2b7919b
Michael Ebens <michael@nova-fusion.com>
2010-12-31 06:48:40 +1100

    Added a bunch of callback snippets and a main.lua template.

---
+++ Templates/main_lua.tmTemplate/main.lua
@@ -0,0 +1,43 @@
+-- Filename: ${TM_FILENAME}
+-- Author: ${TM_FULLNAME}
+-- Date: ${TM_DATE}
+
+-- Initialization
+function load()
+	
+end
+
+-- Logic
+function update(dt)
+	
+end
+
+-- Rendering
+function draw()
+	
+end
+
+-- Input
+function keypressed()
+	
+end
+
+function keyreleased()
+	
+end
+
+function mousepressed()
+	
+end
+
+function mousereleased()
+	
+end
+
+function joystickpressed()
+	
+end
+
+function joystickreleased()
+	
+end
